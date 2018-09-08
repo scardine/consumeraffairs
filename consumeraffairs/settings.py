f90f14f3aaa1bf22cadf55b8c37a41d2f68a203c
Paulo Scardine <paulo@telnyx.com>
2018-09-08 03:34:52 -0300

    Add tests

--- consumeraffairs/settings.py
+++ consumeraffairs/settings.py
@@ -124,10 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import path, include, reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from reviews.models import Company, Review
+
+LOREM = (
+    "Tagclouds, applications engineer compelling seamless communities share "
+    "syndicate enable user-centric, utilize envisioneer orchestrate integrate."
+    " Seamless out-of-the-box brand user-centred 24/365 scalable extensible,"
+    " expedite reinvent extend paradigms. Recontextualize vertical, engage "
+    "innovate standards-compliant world-class incubate plug-and-play "
+    "experiences. One-to-one ecologies reintermediate, synthesize: users "
+    "transition web services wikis. Cross-media blogging incentivize, "
+    "reinvent end-to-end iterate; portals eyeballs e-services enable "
+    "synergies mission-critical matrix: granular e-services networks "
+    "open-source."
+)
+
+
+class ReviewsAPITestCase(APITestCase):
+    urlpatterns = [
+        path('api/v1/', include('reviews.urls')),
+    ]
+
+    def setUp(self):
+        self.root = User.objects.create_superuser("root", "root@eruditorun.org", "cacc")
+        self.user = User.objects.create_user("john", "john@eruditorun.org", "cacc")
+        self.company = Company.objects.create(name="ConsumerAffairs")
+        Review.objects.create(
+            author=self.root,
+            title="Root's Review",
+            summary=LOREM,
+            rating=5,
+            company=self.company,
+            ip="127.0.0.1",
+        )
+        Review.objects.create(
+            author=self.user,
+            title="John's Review",
+            summary=LOREM,
+            rating=3,
+            company=self.company,
+            ip="127.0.0.1",
+        )
+
+    def test_create_review(self):
+        url = reverse('review-list')
+        self.client.login(username='root', password='cacc')
+        response = self.client.post(url, data={
+            "title": "Best Company Ever",
+            "summary": LOREM,
+            "rating": 5,
+            "company": 1,
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_user_cant_read_others_review(self):
+        url = reverse('review-detail', kwargs={"pk": 1})
+        self.client.login(username='john', password='cacc')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        url = reverse('review-detail', kwargs={"pk": 2})
+        self.client.login(username='root', password='cacc')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_user_cant_read_own_review(self):
+        url = reverse('review-detail', kwargs={"pk": 2})
+        self.client.login(username='john', password='cacc')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        url = reverse('review-detail', kwargs={"pk": 1})
+        self.client.login(username='root', password='cacc')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
