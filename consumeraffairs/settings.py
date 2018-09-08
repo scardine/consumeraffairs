e7b23f7e705ad4446a27ee8c7000ebc300032b88
Paulo Scardine <paulo@telnyx.com>
2018-09-08 11:08:50 -0300

    Add pagination.

--- consumeraffairs/settings.py
+++ consumeraffairs/settings.py
@@ -133,6 +133,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
