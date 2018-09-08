958cab1e645f486940b8d288364fcfbcfc4d5cf9
Paulo Scardine <paulo@telnyx.com>
2018-09-08 09:44:26 -0300

    Admin tidy

--- reviews/models.py
+++ reviews/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
 
 class Review(models.Model):
     RATING_VALIDATORS = [
