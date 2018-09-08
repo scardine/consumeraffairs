fbf22318dd837827ba1faff9c1a2d42234e247af
Paulo Scardine <paulos@xtend.com.br>
2018-09-08 09:52:40 -0300

    Add dunder-str method to Company

--- reviews/models.py
+++ reviews/models.py
@@ -5,8 +5,11 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=64)
-
-
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Review(models.Model):
     RATING_VALIDATORS = [
         validators.MaxValueValidator(5, "Maximun rating is 5"),
